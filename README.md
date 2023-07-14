a78516d1a5ff73326e8e3204b7749195a94dcd98
function2 <function2-llx@outlook.com>
2023-07-14 16:03:12 +0800

    update create env

--- README.md
+++ README.md
@@ -5,7 +5,5 @@
 ```zsh
 git clone --recursive git@github.com:function2-llx/PUMT.git <dir>
 cd <dir>
-mamba env create -n <env> -f environment.yaml
-mamba activate <env>
-echo "export PYTHONPATH=$PWD/third-party/LuoLib:\$PYTHONPATH" > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+./create-env.zsh
 ```
---
+++ create-env.zsh
@@ -0,0 +1,10 @@
+#! /bin/zsh
+
+local env_file=$CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+
+mamba env create -n pumt -f environment.yaml
+mamba activate pumt
+echo "export PYTHONPATH=$PWD/third-party/LuoLib:\$PYTHONPATH" > $env_file
+export BUILD_MONAI=1
+echo "export BUILD_MONAI=1" >> $env_file
+pip install --no-build-isolation -e third-party/MONAI
