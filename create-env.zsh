3ae08f76c603ed75a4ef9c6718bf95de97c9d31e
function2 <function2-llx@outlook.com>
2023-07-14 16:11:05 +0800

    create-env.zsh: fix env_file

--- create-env.zsh
+++ create-env.zsh
@@ -1,9 +1,9 @@
 #! /bin/zsh
 
-local env_file=$CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
 mamba env create -n pumt -f environment.yaml
 mamba activate pumt
+local env_file=$CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 echo "export PYTHONPATH=$PWD/third-party/LuoLib:\$PYTHONPATH" > $env_file
 export BUILD_MONAI=1
 echo "export BUILD_MONAI=1" >> $env_file
