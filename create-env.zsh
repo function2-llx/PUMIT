f7aa4e4139f76f785bccee7f3aade868081ef94e
function2 <function2-llx@outlook.com>
2023-07-20 10:03:49 +0800

    update create-env

--- create-env.zsh
+++ create-env.zsh
@@ -2,8 +2,4 @@
 
 mamba env create -n pumt -f environment.yaml
 mamba activate pumt
-local env_file=$CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo "export PYTHONPATH=$PWD/third-party/LuoLib:\$PYTHONPATH" > $env_file
-export BUILD_MONAI=1
-echo "export BUILD_MONAI=1" >> $env_file
-pip install --no-build-isolation -e third-party/MONAI
+BUILD_MONAI=1 pip install --no-build-isolation -e third-party/MONAI
