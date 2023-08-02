26b1a51ee395d0c1096be4f11ec01eeb0e85cfc7
function2 <function2-llx@outlook.com>
2023-08-02 16:44:18 +0800

    update sharpen parameters

---
+++ scripts/model/mim-b-s.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+set -x
+
+python scripts/model/main.py fit \
+    -c conf/model/main.yaml \
+    --model conf/model/vit-b.yaml \
+    --model conf/model/mim-b.yaml \
+    --model.eva02_pretrained_path pre-trained/eva02_B_pt_in21k_p14.pt \
+    --model.tokenizer conf/tokenizer/simple/pre-trained.yaml \
+    --model.tokenizer.path pre-trained/simple-sharpen.ckpt \
+    --model.tokenizer.model.quantize.temperature 0.1 \
+    --data conf/model/cuda.yaml \
+    $@
--- scripts/tokenizer/sharpen.zsh
+++ scripts/tokenizer/sharpen.zsh
@@ -5,5 +5,6 @@ set -x
 python scripts/tokenizer/main.py \
     -c conf/tokenizer/simple/main-sharpen.yaml \
     --model.model.quantize.temperature 0.1 \
+    --loss.quant_weight -0.1 \
     --data.trans_conf.isotropic_th 1.5 \
     $@
