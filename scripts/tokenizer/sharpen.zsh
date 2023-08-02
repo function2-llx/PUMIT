d99b817a37e711d9363ae349663871b088d53900
function2 <function2-llx@outlook.com>
2023-08-02 17:12:43 +0800

    fix loadding pre-trained discriminator

--- scripts/tokenizer/sharpen.zsh
+++ scripts/tokenizer/sharpen.zsh
@@ -4,7 +4,8 @@ set -x
 
 python scripts/tokenizer/main.py \
     -c conf/tokenizer/simple/main-sharpen.yaml \
-    --model.model.quantize.temperature 0.1 \
-    --loss.quant_weight=-3e-2 \
+    --model.quantize.temperature 0.1 \
+    --loss.quant_weight=-0.1 \
     --data.trans_conf.isotropic_th 1.5 \
+    --pretrained_ckpt_path pre-trained/simple.ckpt \
     $@
