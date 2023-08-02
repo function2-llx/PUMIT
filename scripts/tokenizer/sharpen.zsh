0711c08f8aceecbe8f6dc96486aa1b5c02f511de
function2 <function2-llx@outlook.com>
2023-08-02 16:03:59 +0800

    update scripts

--- scripts/tokenizer/sharpen.zsh
+++ scripts/tokenizer/sharpen.zsh
@@ -4,6 +4,6 @@ set -x
 
 python scripts/tokenizer/main.py \
     -c conf/tokenizer/simple/main-sharpen.yaml \
-    --model.quantize.temperature 0.1 \
+    --model.model.quantize.temperature 0.1 \
     --data.trans_conf.isotropic_th 1.5 \
     $@
