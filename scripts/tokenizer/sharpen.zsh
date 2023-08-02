7c7f2ff72c5387c17ac3baa324636c65d01937af
function2 <function2-llx@outlook.com>
2023-08-02 20:05:29 +0800

    remove temperature in sharpen script

--- scripts/tokenizer/sharpen.zsh
+++ scripts/tokenizer/sharpen.zsh
@@ -4,7 +4,6 @@ set -x
 
 python scripts/tokenizer/main.py \
     -c conf/tokenizer/simple/main-sharpen.yaml \
-    --model.quantize.temperature 0.1 \
     --loss.quant_weight=-0.1 \
     --data.trans_conf.isotropic_th 1.5 \
     --training.pretrained_ckpt_path pre-trained/simple.ckpt \
