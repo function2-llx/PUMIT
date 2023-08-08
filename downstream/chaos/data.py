45e10bb72f1e4f142e3df3a6ab4dafbec4ec7879
function2 <function2-llx@outlook.com>
2023-08-08 20:41:59 +0800

    update MONAI, CHAOS

--- downstream/chaos/data.py
+++ downstream/chaos/data.py
@@ -1,8 +1,15 @@
 from pathlib import Path
+import itertools as it
+
 from PIL import Image
+from lightning import LightningDataModule
 import numpy as np
 
+from luolib.types import tuple3_t
+from monai import transforms as mt
 from monai.config import PathLike
+from monai.data import CacheDataset
+from monai.utils import GridSampleMode
 
 mr_mapping = {
     0: 0,
@@ -22,3 +29,51 @@ def read_label(label_dir: PathLike):
     img = np.vectorize(mr_mapping.get)(img)
     img = np.flip(np.rot90(img), 0)
     return img.astype(np.uint8)
+
+class CHAOSDataModule(LightningDataModule):
+    def __init__(self, *, num_fg_classes: int = 4, patch_size: tuple3_t[int], spacing: tuple3_t[float]):
+        super().__init__()
+        self.num_fg_classes = num_fg_classes
+        self.sample_size = patch_size
+        self.spacing = spacing
+
+    @property
+    def train_transform(self):
+        class_indices_postfix = '_cls_indices'
+        return mt.Compose(
+            [
+                mt.LoadImageD(['image', 'label'], image_only=True, ensure_channel_first=True),
+                mt.OrientationD(['image', 'label'], 'SAR'),
+                mt.ScaleIntensityRangePercentilesD('image', 0.5, 99.5, 0., 1., clip=True, channel_wise=True),
+                mt.NormalizeIntensityD('image'),
+                mt.ClassesToIndicesD('label', class_indices_postfix, self.num_fg_classes + 1),
+                mt.SpacingD(
+                    ['image', 'label'], self.spacing,
+                    mode=[GridSampleMode.BILINEAR, GridSampleMode.NEAREST],
+
+                ),
+                mt.OneOf([
+                    mt.RandSpatialCropD(['image', 'label'], self.sample_size, random_center=True, random_size=False),
+                    mt.RandCropByLabelClassesD(
+                        ['image', 'label'], 'label',
+                        self.sample_size,
+                        [0, *it.repeat(1 / self.num_fg_classes, self.num_fg_classes)],
+                        self.num_fg_classes + 1,
+                        indices_key=f'label{class_indices_postfix}',
+                    ),
+                ]),
+                mt.RandAffineD(),
+            ],
+            lazy=True,
+        )
+
+    def train_dataloader(self):
+        data = [
+            {
+                'image': data_dir / 'image.nii.gz',
+                'label': data_dir / 'label.nii.gz',
+            }
+            for data_dir in Path('downstream/data/CHAOS/Train').glob('*/*')
+        ]
+
+        dataset = CacheDataset(data)
--- downstream/chaos/model.py
+++ downstream/chaos/model.py
@@ -3,17 +3,24 @@ from collections.abc import Sequence
 from lightning import LightningModule
 import torch
 from torch import nn
+from torch.nn import functional as nnf
 
 from luolib.models.decoders.full_res import FullResAdapter
+from monai.losses import DiceCELoss
+
 from pumt.model import SimpleViTAdapter
 
 class CHAOSModel(LightningModule):
+    loss_weight: torch.Tensor
+
     def __init__(
         self,
+        *,
         backbone: SimpleViTAdapter,
         decoder: FullResAdapter,
         seg_feature_channels: Sequence[int],
         num_fg_classes: int = 4,
+        loss: DiceCELoss | None = None,
     ):
         super().__init__()
         self.backbone = backbone
@@ -22,6 +29,9 @@ class CHAOSModel(LightningModule):
             nn.Conv3d(c, num_fg_classes + 1, 1)
             for c in seg_feature_channels
         ])
+        self.loss = loss
+        loss_weight = torch.tensor([0.5 ** i for i in range(len(self.seg_heads))])
+        self.register_buffer('loss_weight', loss_weight / loss_weight.sum(), persistent=False)
 
     def forward(self, x: torch.Tensor):
         feature_maps = self.backbone(x)
@@ -30,3 +40,26 @@ class CHAOSModel(LightningModule):
             seg_head(feature_map)
             for seg_head, feature_map in zip(self.seg_heads, seg_feature_maps)
         ]
+
+    def compute_loss(self, output_logits: list[torch.Tensor] | torch.Tensor, seg_label: torch.Tensor):
+        if isinstance(output_logits, list):
+            seg_loss = torch.stack([
+                self.seg_loss_fn(
+                    nnf.interpolate(output_logit, seg_label.shape[2:], mode='trilinear'),
+                    seg_label,
+                )
+                for output_logit in output_logits
+            ])
+            return seg_loss[0], torch.dot(seg_loss, self.loss_weight)
+        else:
+            return self.seg_loss_fn(output_logits, seg_label)
+
+    def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], *args, **kwargs):
+        img, label = batch
+        logits = self(img)
+        loss = torch.stack([
+            self.loss(nnf.interpolate(logit, label.shape[2:], mode='trilinear'), label)
+            for logit in logits
+        ])
+        loss = torch.dot(loss, self.loss_weight)
+        return loss
