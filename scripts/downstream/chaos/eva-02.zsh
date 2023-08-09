2cd44c3cae44626dbe1f56b14e7f63d44511f799
function2 <function2-llx@outlook.com>
2023-08-09 23:34:54 +0800

    update chaos scripts

--- scripts/downstream/chaos/eva-02.zsh
+++ scripts/downstream/chaos/eva-02.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 python scripts/downstream/chaos/main.py fit \
-    -c conf/downstream/chaos/main.yaml \
+    -c conf/downstream/chaos/fit.yaml \
     --data.num_workers 8 \
     --model.backbone conf/model/vit-b.yaml \
     --model.backbone.eva02_pretrained_path pre-trained/eva02_B_pt_in21k_p14.pt \
