fix BTCV validation normalization

downstream/btcv/model.py
@@ -47,6 +47,9 @@ class BTCVModel(LightningModule):
         self.sw_overlap = sw_overlap
         self.dice_metric = DiceMetric(num_classes=num_fg_classes + 1)
 
+    def input_norm(self, x: torch.Tensor):
+        return 2 * x - 1
+
     def get_seg_feature_maps(self, x: torch.Tensor):
         feature_maps = self.backbone(x)
         seg_feature_maps = self.decoder(feature_maps, x)[::-1]
@@ -54,7 +57,7 @@ class BTCVModel(LightningModule):
 
     def training_step(self, batch: tuple[torch.Tensor, ...], *args, **kwargs):
         img, label = batch
-        img = img * 2 - 1
+        img = self.input_norm(img)
         seg_feature_maps = self.get_seg_feature_maps(img)
         logits = [
             seg_head(feature_map)
@@ -70,12 +73,15 @@ class BTCVModel(LightningModule):
 
     def forward(self, x: torch.Tensor):
         seg_feature_map = self.get_seg_feature_maps(x)[0]
-        return self.seg_heads[0](seg_feature_map).softmax(dim=1)
+        return self.seg_heads[0](seg_feature_map)
 
-    def sw_infer(self, x: torch.Tensor):
-        return sliding_window_inference(
+    def sw_infer(self, x: torch.Tensor, softmax: bool = True):
+        y = sliding_window_inference(
             x, self.sample_size, self.sw_batch_size, self, self.sw_overlap, BlendMode.GAUSSIAN,
         )
+        if softmax:
+            y = y.softmax(dim=1)
+        return y
 
     def tta_infer(self, x: torch.Tensor):
         tta_flips = [[], [2], [3], [4], [2, 3], [2, 4], [3, 4], [2, 3, 4]]
@@ -96,7 +102,7 @@ class BTCVModel(LightningModule):
 
     def validation_step(self, batch: tuple[torch.Tensor, ...], *args, **kwargs):
         img, label = batch
-        img = img * 2 - 1
+        img = self.input_norm(img)
         prob = self.sw_infer(img)
         prob = resample(prob, label.shape[2:])
         pred = prob.argmax(dim=1, keepdim=True)

pumt/model/adapter.py
@@ -10,7 +10,6 @@ from .vit import ViT
 class SimpleViTAdapter(ViT):
     def __init__(
         self,
-        aniso_d: int,
         out_indexes: Sequence[int],
         *args, **kwargs,
     ):
@@ -19,8 +18,7 @@ class SimpleViTAdapter(ViT):
         patch_size = self.patch_embed.patch_size
         assert patch_size[1] == patch_size[2] == 16
         assert patch_size[0] & patch_size[0] - 1 == 0
-        assert patch_size[0] == max(1, 16 >> aniso_d)
-        self.aniso_d = aniso_d
+        aniso_d = max(0, (16 // patch_size[0]).bit_count() - 1)
         assert not self.patch_embed.adaptive
         get_args = lambda i: ((1 if aniso_d >= i else 2, 2, 2), ) * 2
         self.fpn = nn.ModuleList([