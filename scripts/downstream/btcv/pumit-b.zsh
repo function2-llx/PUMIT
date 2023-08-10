08b67dbd4c7e1c0d5cb8a692de2482eeeaf1fa13
function2 <function2-llx@outlook.com>
2023-08-10 18:39:40 +0800

    update scripts

--- scripts/downstream/btcv/pumit-b.zsh
+++ scripts/downstream/btcv/pumit-b.zsh
@@ -5,5 +5,5 @@ set -x
 python scripts/downstream/btcv/main.py fit \
     -c conf/downstream/btcv/fit.yaml \
     --model.backbone conf/model/vit-b.yaml \
-    --model.backbone conf/model/eva02-b-ckpt.yaml \
+    --model.backbone.pretrained_ckpt.path pre-trained/pumit-b.ckpt \
     $@
--- scripts/downstream/chaos/pumit-b.zsh
+++ scripts/downstream/chaos/pumit-b.zsh
@@ -5,5 +5,5 @@ set -x
 python scripts/downstream/chaos/main.py fit \
     -c conf/downstream/chaos/fit.yaml \
     --model.backbone conf/model/vit-b.yaml \
-    --model.backbone conf/model/eva02-b-ckpt.yaml \
+    --model.backbone.pretrained_ckpt.path pre-trained/pumit-b.ckpt \
     $@
