4f4f22c9ec8ec6c604ac08733a3db358c34e219b
function2 <function2-llx@outlook.com>
2023-08-12 12:27:25 +0800

    fix BTCV test script

--- scripts/downstream/btcv/test-b.zsh
+++ scripts/downstream/btcv/test-b.zsh
@@ -2,7 +2,7 @@
 
 set -x
 
-python scripts/downstream/btcv/main.py predict \
+python scripts/downstream/btcv/main.py test \
     -c conf/downstream/btcv/test.yaml \
     --model.backbone conf/model/vit-b.yaml \
     $@
